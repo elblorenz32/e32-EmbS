faf9527b936cad8f4f521aa4182a8761e02bf5a0
Timo Buschmann <timo.buschmann@peergroup.com>
2023-12-04 23:13:21 +0100

    qf

--- src/raspi.py
+++ src/raspi.py
@@ -282,8 +282,11 @@ def main():
 	# 	time.sleep(20)
 	blockI2C()
 	setAddr(1)
-	write_str("test", ret=False)
+	write_str("test", ret=True)
 	print(read_str(ret=True))
+	write_int(0b1010101010101010)
+	setAddr(0)
+	print(bin(read_int()))
 	unblockI2C()
 
 if __name__ == "__main__":
